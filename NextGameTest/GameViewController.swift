a69365d9c58beb7815bed819012721de21599c69
badasme <glebreutov@gmail.com>
2021-08-05 16:54:39 +0300

    first commit

--- NextGameTest/GameViewController.swift
+++ NextGameTest/GameViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
 
@@ -16,13 +15,14 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            let scene = GameScene(size: view.bounds.size)
+            scene.backgroundColor = .darkGray
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            scene.scaleMode = .resizeFill
+            
+            // Present the scene
+            view.presentScene(scene)
+            
             
             view.ignoresSiblingOrder = true
             
@@ -35,13 +35,6 @@ class GameViewController: UIViewController {
         return true
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
 
     override var prefersStatusBarHidden: Bool {
         return true
